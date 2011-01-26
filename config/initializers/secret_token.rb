cfcc33bee411a2f885aba98b2c3f967a04203dff
Alex Rudy <alex.rudy@gmail.com>
2011-01-26 13:37:48 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dining47::Application.config.secret_token = '34f6cd9527cc14425f419e200e08a450cc2c4d193d8d74f060f241e8432046d6932970ccd3cd59a627637ecc8fa213492068a91b480d8ab91af737d27a9254ec'
